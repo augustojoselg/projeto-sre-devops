ad75638589d28979adb29034f9bc8ad73bdeda72
augustojoselg <augustojoselg@gmail.com>
2025-08-18 13:30:11 -0300

    fix(tf): Remove outputs for deleted service account key

--- Terraform/service_account/outputs.tf
+++ Terraform/service_account/outputs.tf
@@ -3,12 +3,3 @@ output "devops_email" {
   value       = google_service_account.devops.email
 }
 
-output "devops_private_key" {
-  description = "Chave privada (codificada em Base64) da conta de serviço DevOps"
-  value       = google_service_account_key.devops_key.private_key
-  sensitive   = true
-}
-
-output "service_account_key_file" {
-  value = local_file.devops_key_file.filename
-}
